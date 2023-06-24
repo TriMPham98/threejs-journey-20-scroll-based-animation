942a95abb1318bd4149e8cd1f241a41af64d8ad1
Tri <trimpham98@gmail.com>
2023-06-23 22:58:56 -0700

    Load gradient texture

--- src/script.js
+++ src/script.js
@@ -26,6 +26,11 @@ const scene = new THREE.Scene();
 /**
  * Objects
  */
+
+// Texture
+const textureLoader = new THREE.TextureLoader();
+const gradientTexture = textureLoader.load("/textures/gradients/3.jpg");
+
 const material = new THREE.MeshToonMaterial({
   color: parameters.materialColor,
 });
