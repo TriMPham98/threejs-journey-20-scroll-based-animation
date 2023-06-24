eea68e947c3c28f8f445c87884cf6532925e48cc
Tri <trimpham98@gmail.com>
2023-06-24 00:54:09 -0700

    Divide cursor pixel by viewport width for proportion and -0.5

--- src/script.js
+++ src/script.js
@@ -131,8 +131,10 @@ cursor.x = 0;
 cursor.y = 0;
 
 window.addEventListener("mousemove", (event) => {
-  cursor.x = event.clientX;
-  cursor.y = event.clientY;
+  cursor.x = (event.clientX / sizes.width) - 0.5;
+  cursor.y = (event.clientY / sizes.height) - 0.5;
+
+  console.log(cursor);
 });
 
 /**
