d094c92f249cbffb78d30f4416d211f3c11012ca
Tri <trimpham98@gmail.com>
2023-06-25 09:40:10 -0700

    Add gsap.to function with parameters to update animation

--- src/script.js
+++ src/script.js
@@ -13,7 +13,7 @@ const parameters = {
 
 gui.addColor(parameters, "materialColor").onChange(() => {
   material.color.set(parameters.materialColor),
-  particlesMaterial.color.set(parameters.materialColor)
+    particlesMaterial.color.set(parameters.materialColor);
 });
 
 /**
@@ -38,7 +38,7 @@ gradientTexture.magFilter = THREE.NearestFilter;
 const material = new THREE.MeshNormalMaterial({
   // color: parameters.materialColor,
   // gradientMap: gradientTexture,
-  wireframe: true
+  wireframe: true,
 });
 
 // Meshes
@@ -75,18 +75,23 @@ const positions = new Float32Array(particlesCount * 3);
 
 for (let i = 0; i < particlesCount; i++) {
   positions[i * 3 + 0] = (Math.random() - 0.5) * 10;
-  positions[i * 3 + 1] = objectsDistance * 0.5 - Math.random() * objectsDistance * sectionMeshes.length;
+  positions[i * 3 + 1] =
+    objectsDistance * 0.5 -
+    Math.random() * objectsDistance * sectionMeshes.length;
   positions[i * 3 + 2] = (Math.random() - 0.5) * 10;
 }
 
 const particlesGeometry = new THREE.BufferGeometry();
-particlesGeometry.setAttribute("position", new THREE.BufferAttribute(positions, 3));
+particlesGeometry.setAttribute(
+  "position",
+  new THREE.BufferAttribute(positions, 3)
+);
 
 // Material
 const particlesMaterial = new THREE.PointsMaterial({
   color: parameters.materialColor,
   sizeAttenuation: true,
-  size: 0.03
+  size: 0.03,
 });
 
 // Points
@@ -163,6 +168,14 @@ window.addEventListener("scroll", () => {
 
   if (newSection != currentSection) {
     currentSection = newSection;
+
+    gsap.to(sectionMeshes[currentSection].rotation, {
+      duration: 1.5,
+      ease: "power2.inOut",
+      x: "+=6",
+      y: "+3",
+      z: "+1.5"
+    });
   }
 });
 
@@ -174,8 +187,8 @@ cursor.x = 0;
 cursor.y = 0;
 
 window.addEventListener("mousemove", (event) => {
-  cursor.x = (event.clientX / sizes.width) - 0.5;
-  cursor.y = (event.clientY / sizes.height) - 0.5;
+  cursor.x = event.clientX / sizes.width - 0.5;
+  cursor.y = event.clientY / sizes.height - 0.5;
 });
 
 /**
@@ -194,13 +207,15 @@ const tick = () => {
 
   const parallaxX = cursor.x * 0.5;
   const parallaxY = -cursor.y * 0.5;
-  cameraGroup.position.x += (parallaxX - cameraGroup.position.x) * 3 * deltaTime;
-  cameraGroup.position.y += (parallaxY - cameraGroup.position.y) * 3 * deltaTime;
+  cameraGroup.position.x +=
+    (parallaxX - cameraGroup.position.x) * 3 * deltaTime;
+  cameraGroup.position.y +=
+    (parallaxY - cameraGroup.position.y) * 3 * deltaTime;
 
   // Animate meshes
   for (const mesh of sectionMeshes) {
-    mesh.rotation.x = elapsedTime * 0.1;
-    mesh.rotation.y = elapsedTime * 0.069;
+    mesh.rotation.x += deltaTime * 0.1;
+    mesh.rotation.y += deltaTime * 0.069;
   }
 
   // Render
